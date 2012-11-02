03afecd2efb8e9e3856f40b220f524660e12f244
Chris Martel <chris@codeways.org>
2012-11-02 23:12:34 +0100

    Initial commit.

---
+++ www/src/Codeways/UserBundle/CodewaysUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Codeways\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CodewaysUserBundle extends Bundle
+{
+}
---
+++ www/src/Codeways/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Codeways\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('CodewaysUserBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
