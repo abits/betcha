efe80c712d27f726e2831e08d17adbe320e6bf6d
Chris Martel <chris@codeways.org>
2012-11-09 23:26:19 +0100

    Added login link to navbar.

--- www/src/Codeways/UserBundle/Controller/DefaultController.php
+++ www/src/Codeways/UserBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace Codeways\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('CodewaysUserBundle:Default:index.html.twig', array('name' => $name));
+
+        $request = new Request();
+        $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        return $this->render('CodewaysUserBundle:Default:index.html.twig',
+            array('error' => $error));
     }
 }
